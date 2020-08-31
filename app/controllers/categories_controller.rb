cc7886db0e95fb467c475d0623cd60c0dd6394b7
Martin Margonari <martinmargonari@gmail.com>
2020-08-31 18:44:35 -0300

    Create Category functionality added

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,31 @@
+class CategoriesController < ApplicationController
+
+  def show
+
+  end
+
+  def index
+
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was created successfully."
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+end
