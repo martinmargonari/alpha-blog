e8c21df1475619e1a4120f04a1c51ef072e84482
Martin Margonari <martinmargonari@gmail.com>
2020-09-06 12:59:47 -0300

    Added many to many associations category and article

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,7 @@ class Category < ApplicationRecord
   validates :name, presence: true,
                    uniqueness: { case_sensitive: false },
                    length: {minimum: 3, maximum: 25}
+  has_many :article_categories
+  has_many :articles, through: :article_categories
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_31_120734) do
+ActiveRecord::Schema.define(version: 2020_09_06_154403) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
